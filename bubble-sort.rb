b6da4a25407bbd568ca196d4e6255a6d333c9201
Andrew <andrew@andrewsmith.org>
2022-07-28 23:25:55 -0400

    Sort only as many times as needed

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,13 +1,24 @@
 def bubble_sort (array)
-    def sort_cycle(array) = array.each_with_index do |num, index|
-        hold = nil
-        if (num <=> array[index+1]) == 1
-            hold = array[index]
-            array[index] = array[index+1]
-            array[index+1] = hold
+
+    def sort_cycle(array)
+        sorted_a_pair = false
+        
+        array.each_with_index do |num, index|
+            hold = nil
+            if (num <=> array[index+1]) == 1
+                hold = array[index]
+                array[index] = array[index+1]
+                array[index+1] = hold
+                sorted_a_pair = true
+            end
         end
+
+        if sorted_a_pair == true
+            sort_cycle(array)
+        end
+
     end
-    array.length.times {sort_cycle(array)}
+    sort_cycle(array)
     array
 end
 
